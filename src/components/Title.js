a961f782512792890d385b7e4eb7d1ffedf185a8
emreharman <emrehrmnb@gmail.com>
2023-01-14 23:57:14 +0300

    ...

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,15 +2,28 @@ import React from "react";
 
 import { useSelector } from "react-redux";
 
-const Title=()=>{
-    const uygulamaninStatei=useSelector(state=>state)
-    return(
-        <div>
-            <h1>TODO APP</h1>
-            <p>Kayıtlı todo sayısı: {uygulamaninStatei.todosState.todos.length}</p>
-            <p>Yapılanlar:{uygulamaninStatei.todosState.todos.filter(item => item.isDone === true).length}     Yapılmayanlar:{uygulamaninStatei.todosState.todos.filter(item => item.isDone === false).length}</p>
-        </div>
-    )
-}
+const Title = () => {
+  const uygulamaninStatei = useSelector((state) => state);
+  return (
+    <div>
+      <h1>TODO APP</h1>
+      <p>Kayıtlı todo sayısı: {uygulamaninStatei.todosState.todos.length}</p>
+      <p>
+        Yapılanlar:
+        {
+          uygulamaninStatei.todosState.todos.filter(
+            (item) => item.isDone === true
+          ).length
+        }{" "}
+        Yapılmayanlar:
+        {
+          uygulamaninStatei.todosState.todos.filter(
+            (item) => item.isDone === false
+          ).length
+        }
+      </p>
+    </div>
+  );
+};
 
-export default Title
+export default Title;
